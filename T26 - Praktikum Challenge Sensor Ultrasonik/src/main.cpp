8916257783c5295cee5b70a7dd796ede8c445191
rizalhimself <rizalhimself@gmail.com>
2022-10-08 12:47:25 +0700

    challenge ultrasonik done

--- T26 - Praktikum Challenge Sensor Ultrasonik/src/main.cpp
+++ T26 - Praktikum Challenge Sensor Ultrasonik/src/main.cpp
@@ -6,7 +6,23 @@ const int pinLedM = D5;
 const int pinLedK = D6;
 const int pinLedH = D7;
 long durasi, jarak;
+unsigned long previousMillis;
+unsigned long previousMillisBz;
+const long interval = 2000;
+const long intervalBz = 2000;
+int ledState = LOW;
+int bzState = LOW;
 
+void dapatkanDataJarak()
+{
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+  durasi = pulseIn(pinEcho, HIGH);
+  jarak = (durasi / 2) / 29.1;
+}
 
 void setup()
 {
@@ -23,19 +39,55 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  
+  dapatkanDataJarak();
   Serial.print("Jarak: ");
   Serial.print(jarak);
   Serial.println(" cm");
   delay(500);
-}
+  if (jarak >= 5)
+  {
+    digitalWrite(pinLedH, HIGH);
+  }
+  else
+  {
+    digitalWrite(pinLedH, LOW);
+  }
 
-void dapatkanDataJarak(){
-  digitalWrite(pinTrigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pinTrigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pinTrigger, LOW);
-  durasi = pulseIn(pinEcho, HIGH);
-  jarak = (durasi / 2) / 29.1;
+  if (jarak >= 9 && jarak <= 11)
+  {
+    digitalWrite(pinLedK, HIGH);
+    tone(pinBuzzer, 800);
+    delay(250);
+    digitalWrite(pinLedK, LOW);
+    noTone(pinBuzzer);
+    delay(250);
+  }
+  else if (jarak > 11)
+  {
+    digitalWrite(pinLedK, HIGH);
+  }
+  else
+  {
+    digitalWrite(pinLedK, LOW);
+  }
+
+  if (jarak >= 14 && jarak <= 15)
+  {
+    digitalWrite(pinLedM, HIGH);
+    tone(pinBuzzer, 900);
+    delay(250);
+    digitalWrite(pinLedM, LOW);
+    noTone(pinBuzzer);
+    delay(250);
+  }
+  else if (jarak > 15)
+  {
+    digitalWrite(pinLedM, HIGH);
+    tone(pinBuzzer, 1000);
+  }
+  else
+  {
+    digitalWrite(pinLedM, LOW);
+    noTone(pinBuzzer);
+  }
 }
