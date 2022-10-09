401b37c20aa50f418379cb5f40b996ff38f51ac1
rizalhimself <rizalhimself@gmail.com>
2022-10-09 22:29:35 +0700

    Coding challenge sensor cahaya finished

---
+++ T26 - Praktikum Challenge Sensor Cahaya/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+#define pinLDR A0
+#define pinLED D8
+
+int valSensor;
+float voltageSensor;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pinLED, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  valSensor = analogRead(pinLDR);
+  voltageSensor = valSensor * (5.0 / 1023.0);
+  Serial.print("Pembacaan Analog: ");
+  Serial.println(valSensor);
+  Serial.print("Pembacaan Voltase: ");
+  Serial.print(voltageSensor);
+  Serial.println(" volt");
+  delay(1000);
+
+  if (valSensor >= 1000)
+  {
+    digitalWrite(pinLED, HIGH);
+  }
+  else
+  {
+    digitalWrite(pinLED, LOW);
+  }
+}
