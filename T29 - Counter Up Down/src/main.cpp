f05855cc87d6e2010a33230f00c8675a315a82cf
rizalhimself <rizalhimself@gmail.com>
2022-10-31 12:46:20 +0700

    T29 Counter Up Down Finished

--- T29 - Counter Up Down/src/main.cpp
+++ T29 - Counter Up Down/src/main.cpp
@@ -1,9 +1,63 @@
 #include <Arduino.h>
+#include <TM1637Display.h>
 
-void setup() {
+#define CLK 19
+#define DIO 18
+#define btIncreasePin 25
+#define btDecreasePin 26
+
+int value = 0;
+int btIncreaseState = 0;
+int btDecreaseState = 0;
+TM1637Display display(CLK, DIO);
+
+const uint8_t allOn[] = {0xff, 0xff, 0xff, 0xff};
+const uint8_t allOff[] = {0x00, 0x00, 0x00, 0x00};
+
+void setup()
+{
   // put your setup code here, to run once:
+  pinMode(btIncreasePin, INPUT);
+  pinMode(btDecreasePin, INPUT);
+  display.setBrightness(7);
+  display.setSegments(allOn);
+  delay(1000);
+  display.setSegments(allOff);
+  delay(1000);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  btDecreaseState = digitalRead(btDecreasePin);
+  btIncreaseState = digitalRead(btIncreasePin);
+
+  if (btIncreaseState == HIGH)
+  {
+    /* code */
+    if (value == 9999)
+    {
+      value = 0;
+    }
+    else
+    {
+      value++;
+    }
+  }
+
+  if (btDecreaseState == HIGH)
+  {
+    /* code */
+    if (value == 0)
+    {
+      value = 9999;
+    }
+    else
+    {
+      value--;
+    }
+  }
+
+  display.showNumberDec(value);
+  delay(200);
 }
