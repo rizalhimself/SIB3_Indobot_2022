725b24342dcf82014a6640c0181dfc1810cd3de7
rizalhimself <rizalhimself@gmail.com>
2022-11-01 04:50:28 +0700

    T29 - LDR OLED Done

---
+++ T29 - Mengukur Intensitas Cahaya dengan Tampilan OLED/src/main.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCAdrress 0x3c
+#define pinLDR 39
+
+int nilaiLDR = 0;
+String kondisi = "";
+
+Adafruit_SSD1306 screen = Adafruit_SSD1306(128,64,&Wire);
+
+void setup() {
+  // put your setup code here, to run once:
+  screen.begin(SSD1306_SWITCHCAPVCC, SCAdrress);
+  screen.display();
+  delay(1000);
+  screen.clearDisplay();
+  screen.display();
+  pinMode(pinLDR, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  nilaiLDR = analogRead(pinLDR);
+
+  if (nilaiLDR >= 3000)
+  {
+    kondisi = "Gelap";
+  } else
+  {
+    kondisi = "Terang";
+  }
+  
+  screen.setTextSize(1);
+  screen.setTextColor(SSD1306_WHITE);
+  screen.setCursor(0,10);
+  screen.print("Intensitas Cahaya");
+  screen.setCursor(0,30);
+  screen.print("Nilai LDR = ");
+  screen.setCursor(75,30);
+  screen.print(nilaiLDR);
+  screen.setCursor(0,40);
+  screen.print("Kondisi = ");
+  screen.setCursor(63,40);
+  screen.print(kondisi);
+  screen.display();
+  delay(1000);
+  screen.clearDisplay();
+}
