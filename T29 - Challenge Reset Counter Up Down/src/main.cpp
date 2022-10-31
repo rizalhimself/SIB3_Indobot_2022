5fb20718f1d44fc56f03767e08841d1caeb21191
rizalhimself <rizalhimself@gmail.com>
2022-10-31 20:05:11 +0700

    T29 Ch Reset Counter Up Down Done

---
+++ T29 - Challenge Reset Counter Up Down/src/main.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+#define CLK 19
+#define DIO 18
+#define btIncreasePin 25
+#define btDecreasePin 26
+
+int value = 0;
+int btCounter = 0;
+int btIncreaseState = 0;
+int btDecreaseState = 0;
+TM1637Display display(CLK, DIO);
+
+const uint8_t allOn[] = {0xff, 0xff, 0xff, 0xff};
+const uint8_t allOff[] = {0x00, 0x00, 0x00, 0x00};
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(btIncreasePin, INPUT_PULLUP);
+  pinMode(btDecreasePin, INPUT_PULLUP);
+  display.setBrightness(7);
+  display.setSegments(allOn);
+  delay(1000);
+  display.setSegments(allOff);
+  delay(1000);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  btDecreaseState = digitalRead(btDecreasePin);
+  btIncreaseState = digitalRead(btIncreasePin);
+
+  if (btIncreaseState == LOW)
+  {
+    /* code */
+    if (value == 9999)
+    {
+      value = 0;
+    }
+    else
+    {
+      value++;
+    }
+  }
+
+  if (btDecreaseState == LOW)
+  {
+    /* code */
+    if (value == 0)
+    {
+      value = 9999;
+    }
+    else
+    {
+      value--;
+    }
+  }
+
+  if (btDecreaseState == LOW && btIncreaseState == LOW)
+  {
+    value = 0;
+  }
+
+  display.showNumberDec(value);
+  delay(200);
+}
