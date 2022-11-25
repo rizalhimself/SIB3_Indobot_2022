a722d709155ebb3f4a698d2211b7b426d59637e6
rizalhimself <rizalhimself@gmail.com>
2022-11-25 17:54:16 +0700

    T43 CH OTA finished

--- T43 - OTA Led Blink/src/main.cpp
+++ T43 - OTA Led Blink/src/main.cpp
@@ -5,7 +5,7 @@
 #define BLYNK_TEMPLATE_ID "TMPL-Yuk1grF"
 #define BLYNK_DEVICE_NAME "43 OTA"
 
-#define BLYNK_FIRMWARE_VERSION "0.1.3"
+#define BLYNK_FIRMWARE_VERSION "0.1.4"
 
 #define BLYNK_PRINT Serial
 //#define BLYNK_DEBUG
@@ -19,15 +19,24 @@
 //#define USE_WEMOS_D1_MINI
 
 #include "BlynkEdgent.h"
+#include "DHT.h"
 
 #define pinLed D7
 #define pinLDR A0
+#define pinDHT D6
+
+#define vPinSwLED V0
+#define vPinNLDR V1
+#define vPinNvoltase V2
+#define vPinNsuhu V3
+#define vPinNkelembapan V4
 
 int pinVal, nilaiLDR;
-float voltase;
+float voltase, suhu, kelembapan;
 BlynkTimer timer;
+DHT dht(DHT11, pinDHT);
 
-BLYNK_WRITE(V0)
+BLYNK_WRITE(vPinSwLED)
 {
   pinVal = param.asInt();
   if (pinVal == 1)
@@ -44,15 +53,35 @@ void sendSensorData()
 {
   nilaiLDR = analogRead(pinLDR);
   voltase = nilaiLDR * (5.0 / 1023.0);
+  suhu = dht.readTemperature();
+  kelembapan = dht.readHumidity();
 
-  Blynk.virtualWrite(V1, nilaiLDR);
-  delay(500);
-  Blynk.virtualWrite(V2, voltase);
+  Blynk.virtualWrite(vPinNLDR, nilaiLDR);
+  Serial.println("Nilai LDR = ");
+  Serial.print(nilaiLDR);
+  Serial.print(" K");
+  delay(250);
+  Blynk.virtualWrite(vPinNvoltase, voltase);
+  Serial.println("Voltase = ");
+  Serial.print(voltase);
+  Serial.print(" Volt");
+  delay(250);
+  Blynk.virtualWrite(vPinNsuhu, suhu);
+  Serial.println("Suhu = ");
+  Serial.print(suhu);
+  Serial.print(" C");
+  delay(250);
+  Blynk.virtualWrite(vPinNkelembapan, kelembapan);
+  Serial.println("Kelembapan = ");
+  Serial.print(kelembapan);
+  Serial.print(" %");
+  delay(250);
 }
 
 void setup()
 {
   Serial.begin(115200);
+  dht.begin();
   pinMode(pinLed, OUTPUT);
   delay(100);
   timer.setInterval(1000L, sendSensorData);
